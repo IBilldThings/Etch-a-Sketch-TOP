8cd85a5cc1ce736b4b1e3549a8d0f1bb29124888
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2026-01-03 20:23:16 -0500

    feat: add opacity-coloring and randomColoring

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const btn = document.querySelector(".btn")
 const grid = document.querySelector(".container");
 let response = 16;
 
-function createGrid(response){
+function randomColor(){ //Randomize color for each square
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
+}
+
+function createGrid(response){ //Generate the grid based on the response number (default: 16)
     grid.innerHTML = "";
 
     const size = 100 / response;
 
     for (let i = 0; i < response * response; i++){
         let square = document.createElement("div");
-        square.style.border = "1px solid black";
         square.style.boxSizing = "border-box";
+        square.style.backgroundColor = randomColor();
 
         square.style.flex = `0 0 ${size}%`;
         square.style.height = `${size}%`;
@@ -20,23 +30,29 @@ function createGrid(response){
     }
 }
 
-function gridColoring(){
+function gridColoring(){ //Color the grid through mouse entry
     let coloring = document.querySelectorAll(".changingDiv");
 
     coloring.forEach(div => {
         div.addEventListener("mouseenter", colorChange);
+        let opacity = 0;
+        div.style.opacity = opacity;
 
         function colorChange(){
-            div.style.backgroundColor = "black";
+            if (opacity != 1){ //Color by slowly increasing opacity
+                opacity += 0.10;
+                div.style.opacity = opacity;
+            }
+            
         }
 
     });
 }
 
-createGrid(response); // Init the grid with 16 squares
-gridColoring();
+createGrid(response); // Init the grid with default 16 squares
+gridColoring(); // Start event for mouse coloring
 
-btn.addEventListener("click", () =>{ // retrieve response
+btn.addEventListener("click", () =>{ // Button to reset and get response
     response = prompt("What size do you want for the grid? (Max: 100) (Min: 1)", 16);
     while (true){
         if (response > 100 || response < 1){
